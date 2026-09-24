56f2610bf796c701ffcdb2ff0d81711e942d1f3d
kolya-makarenko <lifestealer27@gmail.com>
2026-09-25 01:18:02 +0300

    fourth change text for asset page

--- src/components/Main/Pages/AssetsPage/AssetsPageTypes/AssetsPageTypes.jsx
+++ src/components/Main/Pages/AssetsPage/AssetsPageTypes/AssetsPageTypes.jsx
@@ -8,47 +8,83 @@ import typesIcon5 from '../../../../../assets/images/assetsPageImages/typesIcon5
 import typesIcon6 from '../../../../../assets/images/assetsPageImages/typesIcon6.svg';
 import typesIcon7 from '../../../../../assets/images/assetsPageImages/typesIcon7.svg';
 import typesIcon8 from '../../../../../assets/images/assetsPageImages/typesIcon8.svg';
+import typesIcon9 from '../../../../../assets/images/assetsPageImages/typesIcon9.svg';
+import typesIcon10 from '../../../../../assets/images/assetsPageImages/typesIcon10.svg';
+import typesIcon11 from '../../../../../assets/images/assetsPageImages/typesIcon11.svg';
+import typesIcon12 from '../../../../../assets/images/assetsPageImages/typesIcon12.svg';
+import typesIcon13 from '../../../../../assets/images/assetsPageImages/typesIcon13.svg';
+import typesIcon14 from '../../../../../assets/images/assetsPageImages/typesIcon14.svg';
 
 const typesBoxes = [
     {
         id: '1',
         icon: typesIcon1,
-        txt: 'Real Estate',
+        txt: 'Technology',
     },
     {
         id: '2',
         icon: typesIcon2,
-        txt: 'Land & Development',
+        txt: 'Real Estate',
     },
     {
         id: '3',
         icon: typesIcon3,
-        txt: 'Operating Businesses',
+        txt: 'Hospitality',
     },
     {
         id: '4',
         icon: typesIcon4,
-        txt: 'Hospitality & Hotels',
+        txt: 'Energy & Infrastructure',
     },
     {
         id: '5',
         icon: typesIcon5,
-        txt: 'Industrial & Logistics',
+        txt: 'Dual-Use Technology',
     },
     {
         id: '6',
         icon: typesIcon6,
-        txt: 'Energy & Infrastructure',
+        txt: ' iGaming',
     },
     {
         id: '7',
         icon: typesIcon7,
-        txt: 'Funds & Portfolios',
+        txt: 'Food & Beverage',
     },
     {
         id: '8',
         icon: typesIcon8,
-        txt: 'IP & Royalty Assets',
+        txt: 'Art & Luxury',
+    },
+    {
+        id: '9',
+        icon: typesIcon9,
+        txt: 'Entertainment',
+    },
+    {
+        id: '10',
+        icon: typesIcon10,
+        txt: 'Industrials',
+    },
+    {
+        id: '11',
+        icon: typesIcon11,
+        txt: 'Land & Development',
+    },
+    {
+        id: '12',
+        icon: typesIcon12,
+        txt: 'Investment Funds',
+    },
+    {
+        id: '13',
+        icon: typesIcon13,
+        txt: 'Agriculture',
+    },
+    {
+        id: '14',
+        icon: typesIcon14,
+        txt: 'Mobility Tech',
     },
 ];
 
